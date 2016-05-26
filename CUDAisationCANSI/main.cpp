e4035b911cd2640aca6a68b6f46e8eda25137341
jasonjamet <jason.jamet@gmail.com>
2016-05-26 18:32:03 +0200

    add main getopt for xml and c

--- CUDAisationCANSI/main.cpp
+++ CUDAisationCANSI/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <iostream>
-// #include <unistd.h>
+#include <iostream>
+#include <unistd.h>
 #include "node.hpp"
 #include "CodeGen.hpp"
 #include "CodeString.hpp"
@@ -9,128 +9,150 @@
 #include "algorithm"
 
 
-// extern FILE *yyin;
+extern FILE *yyin;
 extern int yyparse();
 extern TranslationUnit* root;
 
 
-//
-// bool checkExtentionFile(char* fileName) {
-// 	if(fileName != NULL) {
-// 		std::string fileNameStr = fileName;
-// 		std::string fileExtention = fileNameStr.substr(fileNameStr.find(".")+1);
-// 		return (fileExtention == "c" || fileExtention == "cpp");
-// 	} else {
-// 		return true;
-// 	}
-//
-// }
-//
-// void generateXmlOutput(char *outputFileName, char *inputFileName) {
-// 	if(outputFileName && outputFileName != inputFileName) {
-// 		std::string outputFileNameStr = outputFileName;
-// 		if(outputFileNameStr.find(".") != std::string::npos) {
-// 			FILE *outFile_p;
-// 			outFile_p=fopen(outputFileName,"w");
-// 			if(!outFile_p) {
-// 				printf("couldn't open temp for writting\n");
-// 				exit(0);
-// 			} else {
-// 				fputs(root->toStdString().c_str(),outFile_p);
-// 				fclose(outFile_p);
-// 			}
-// 		}
-// 	}
-// 	std::cout << root->toStdString() << std::endl;
-// }
-//
-// void generateCcodeOutput(char *outputFileName, char *inputFileName) {
-// 	CodeBlock *context = new CodeBlock();
-// 	root->toPrettyCode(context);
-//
-// 	if(outputFileName && outputFileName != inputFileName) {
-// 		std::string outputFileNameStr = outputFileName;
-// 		if(outputFileNameStr.find(".") != std::string::npos) {
-//
-// 			FILE *outFile_p;
-// 			outFile_p=fopen(outputFileName,"w");
-// 		  if(!outFile_p) {
-// 		   printf("couldn't open temp for writting\n");
-// 		   exit(0);
-// 		  } else {
-// 				fputs(context->toPrettyCode().c_str(),outFile_p);
-// 				fclose(outFile_p);
-// 			}
-// 		}
-// 	}
-// 	std::cout << context->toPrettyCode();
-// }
 
-int main(int argc, char **argv) {
-	// int opt = 0;
-	// if(argc < 2) {
-	// 	std::cout << "Please specify an input file" << std::endl;
-	// 	return 1;
-	// } else {
-	// 	if(checkExtentionFile(argv[argc-1])) {
-	// 		yyin=fopen(argv[argc-1],"r");
-	// 		if(!yyin) {
-	// 			std::cout << "Couldn't open file" << std::endl;
-	// 			return 1;
-	// 		} else {
-	// 			if(yyparse()) {
-	// 				integrityTest();
-	// 				while ((opt = getopt(argc, argv, "x:c:")) != -1) {
-	// 					switch(opt) {
-	// 						case 'x':
-	// 							generateXmlOutput(optarg, argv[argc-1]);
-	// 							break;
-	// 						case 'c':
-	// 							generateCcodeOutput(optarg, argv[argc-1]);
-	// 							break;
-	// 						case '?':
-	// 						if (optopt == 'i') {
-	// 							printf("\nMissing mandatory input option");
-	// 						} else if (optopt == 'o') {
-	// 							 printf("\nMissing mandatory output option");
-	// 						} else {
-	// 							 printf("\nInvalid option received");
-	// 						}
-	// 						break;
-	// 					}
-	//
-	// 				}
-	// 			} else {
-	// 					std::cout << "[FAIL] Error during the parse of the file." << std::endl;
-	// 			}
-	// 			fclose(yyin);
-	// 		}
-	//
-	// 	} else {
-	// 		std::cout << "Please specify a C or C++ input file" << std::endl;
-	// 	}
-	// }
-
-	//yyin=fopen("tests/simple.c","r");
-	int parse_result = yyparse();
-	char *PARSE_CODE = getenv ("PARSE_CODE");
-	char *PARSE_AST = getenv ("PARSE_AST");
-
-	if(parse_result == 0){
-		integrityTest();
-  		if (PARSE_AST != NULL){
-			/** will print the ast in std string */
-			std::cout << root->toStdString() << std::endl;
-		}
-
-		if (PARSE_CODE != NULL){
-			/** will print pretty code */
-			CodeBlock *context = new CodeBlock();
-			root->toPrettyCode(context);
-			std::cout << context->toPrettyCode();
-		}
-
+bool checkExtentionFile(char* fileName) {
+	if(fileName != NULL) {
+		std::string fileNameStr = fileName;
+		std::string fileExtention = fileNameStr.substr(fileNameStr.find(".")+1);
+		return (fileExtention == "c" || fileExtention == "cpp");
+	} else {
+		return true;
 	}
 
-	return parse_result;
+}
+
+void generateXmlOutput(std::string outputFileName, bool show) {
+	if(outputFileName != "") {
+		if(outputFileName.find(".") != std::string::npos) {
+			FILE *outFile_p;
+			outFile_p=fopen(outputFileName.c_str(),"w");
+			if(outFile_p == NULL) {
+				printf("couldn't open temp for writting\n");
+				exit(0);
+			} else {
+				fputs(root->toStdString().c_str(),outFile_p);
+				fclose(outFile_p);
+			}
+		}
+	}
+	if(show) {
+		std::cout << root->toStdString() << std::endl;
+	}
+}
+
+void generateCcodeOutput(std::string outputFileName, bool show) {
+	CodeBlock *context = new CodeBlock();
+	root->toPrettyCode(context);
+
+	if(outputFileName != "") {
+		FILE *outFile_p;
+		outFile_p=fopen(outputFileName.c_str(),"w");
+	  if(outFile_p == NULL) {
+	   printf("couldn't open temp for writting\n");
+	   exit(0);
+	  } else {
+			fputs(context->toPrettyCode().c_str(),outFile_p);
+			fclose(outFile_p);
+		}
+	}
+	if(show) {
+		std::cout << context->toPrettyCode();
+	}
+}
+
+void help() {
+	std::cout	<< "Transforms a \"C\" function in a \"CUDA\" function." << std::endl
+	<< "Usage: parser [OPTION] [FILE]" << std::endl
+	<< std::endl
+	<< "Example: parser main.c" << std::endl
+	<< "-x OUTPUT_FILE \t\t Print an XML representation of the tree (build from ANSI-C)." << std::endl
+	<< "-c OUTPUT_FILE \t\t Print the C code with cuda kernel(s)." << std::endl;
+
+}
+
+
+int main(int argc, char **argv) {
+	int opt = 0;
+	bool showXml = false;
+	bool showC = false;
+	std::string outputFileName = "";
+
+
+	while ((opt = getopt(argc, argv, "xco:h")) != -1) {
+		switch(opt) {
+			case 'x':
+				showXml = true;
+				break;
+			case 'c':
+				showC = true;
+				break;
+			case 'o':
+				if(optarg == NULL || optarg == argv[argc-1]) {
+					std::cout << "Option -o requires an argument." << std::endl;
+					return 1;
+				} else {
+					std::string outputFileNameStr = optarg;
+					if(outputFileNameStr.find("-") != std::string::npos) {
+						std::cout << "Option -o requires an argument." << std::endl;
+						return 1;
+					}
+					outputFileName = optarg;
+				}
+				break;
+			case 'h':
+			case '?':
+				if (optopt == 'o') {
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				}
+				else if (isprint (optopt)) {
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				}
+				help();
+				return 1;
+			default:
+				help();
+				return 1;
+		}
+	}
+
+	if(argc < 2) {
+		std::cout << "Please specify an input file" << std::endl;
+		return 1;
+	} else {
+		if(checkExtentionFile(argv[argc-1])) {
+			yyin=fopen(argv[argc-1],"r+");
+			if(yyin == NULL) {
+				std::cout << "Couldn't open file" << std::endl;
+				return 1;
+			} else {
+				if(yyparse() == 0) {
+					integrityTest();
+
+					if(showC && showXml && outputFileName != "") {
+						generateXmlOutput("", true);
+						generateCcodeOutput(outputFileName.c_str(), true);
+					} else if(!showC && !showXml && outputFileName == "") {
+						generateCcodeOutput("", true);
+					} else {
+						generateXmlOutput(outputFileName.c_str(), showXml);
+						generateCcodeOutput(outputFileName.c_str(), showC);
+
+					}
+				} else {
+						std::cout << "Error during the parse of the file." << std::endl;
+				}
+				fclose(yyin);
+			}
+
+		} else {
+			std::cout << "Please specify a C or C++ input file" << std::endl;
+		}
+	}
+
+	return 0;
 }
