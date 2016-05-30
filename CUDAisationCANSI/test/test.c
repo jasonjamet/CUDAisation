b29648c23f573a8a2fc2a4d207833e89e5502334
Guillaume <grandjean.guillaume1@gmail.com>
2016-05-30 19:19:53 +0200

    thread_block

--- CUDAisationCANSI/test/test.c
+++ CUDAisationCANSI/test/test.c
@@ -3,7 +3,7 @@ int y2 = 2;
 
 
 
-#pragma cuda thread_loop(j) block_size(x1,y2) grid_size(16,16)
+#pragma cuda thread_loop(j) block_size(4,2,3) grid_size(15,15,15)
 void sum(float *x, float *y, float *z, int size) {
  int i;
  for(i=0; i<size; ++i) {
@@ -16,7 +16,7 @@ void sum(float *x, float *y, float *z, int size) {
 }
 
 int main(int argc, char *argv[]) {
-  int vector_size = 10;
+  /*int vector_size = 10;
   float h_x[vector_size], h_y[vector_size], h_z[vector_size];
 
   for (int i = 0; i < vector_size; i++) {
@@ -25,6 +25,6 @@ int main(int argc, char *argv[]) {
 	}
 
   sum(h_x, h_y, h_z, vector_size);
-  
+  */
   return 0;
 }
